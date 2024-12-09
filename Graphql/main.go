c973a077b9d0a62bc4c12c0836c12988ceffe116
kckurkcu1 <100448747+kckurkcu1@users.noreply.github.com>
2024-12-09 10:59:28 +0300

    x

--- Graphql/main.go
+++ Graphql/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/kelseyhightower/envconfig"
+	"log"
+	"net/http"
+)
+
+type AppConfig struct {
+	AccountUrl string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogUrl string `envconfig:"CATALOG_SERVICE_URL"`
+	OrderUrl   string `envconfig:"ORDER_SERVICE_URL"`
+}
+
+func main() {
+	var cfg AppConfig
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := NewGraphQLServer(cfg.AccountUrl, cfg.CatalogUrl, cfg.OrderUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/graphql", handler.GraphQL(s.ToExecutableSchema()))
+	http.Handle("/playground", playground.Handler("Kerem", "/graphql"))
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
